638e845bde8a65c6b74439536bdc654c014460a0
apqp2378 <apqp2378@gmail.com>
2026-06-25 03:11:22 +0900

    [프로그래머스] 중급 - 우유와 요거트가 담긴 장바구니Summer/Winter Coding(2019) (#65)

---
+++ programmers/3_중급/65_우유와_요거트가_담긴_장바구니Summer_Winter_Coding(2019).sql
@@ -0,0 +1,12 @@
+-- 우유와 요거트가 담긴 장바구니Summer/Winter Coding(2019)
+-- 프로그래머스 중급 (⭐⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/62284
+-- 작성자: 최상원
+-- 작성일: 2026. 06. 25. 03:11:21
+
+SELECT CART_ID
+FROM CART_PRODUCTS
+WHERE NAME IN ('Milk', 'Yogurt')
+GROUP BY CART_ID
+HAVING COUNT(DISTINCT NAME) = 2
+ORDER BY CART_ID;
