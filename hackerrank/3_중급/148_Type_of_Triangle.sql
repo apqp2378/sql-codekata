653a89fb9eac1c14c714613b4b236e07ebc905d5
apqp2378 <apqp2378@gmail.com>
2026-09-11 16:37:03 +0900

    [HackerRank] 중급 - Type of Triangle (#148)

---
+++ hackerrank/3_중급/148_Type_of_Triangle.sql
@@ -0,0 +1,8 @@
+-- Type of Triangle
+-- HackerRank 중급 (⭐⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+-- 작성자: 최상원
+-- 작성일: 2026. 09. 11. 16:37:05
+
+        END
+FROM triangles
