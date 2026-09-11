f1f8a74726cdc5f0c6233261a503490991c679e6
apqp2378 <apqp2378@gmail.com>
2026-09-11 16:24:24 +0900

    [HackerRank] 고급 - Symmetric Pairs (#178)

---
+++ hackerrank/4_고급/178_Symmetric_Pairs.sql
@@ -0,0 +1,9 @@
+-- Symmetric Pairs
+-- HackerRank 고급 (⭐⭐⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/symmetric-pairs/problem
+-- 작성자: 최상원
+-- 작성일: 2026. 09. 11. 16:24:26
+
+    JOIN functions as f2 on f1.X = f2.Y and f1.Y = f2.X
+where f1.X < F1.Y
+ORDER BY X
